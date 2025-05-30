14bf3f1ab9dcc25f5d9681c2e34fd3f5ffe8aad8
Caio Vellani <caiovellani@outlook.com>
2025-05-29 22:29:03 -0300

    feat: Add 2-second delay to user fetch operation

--- src/redux/user/saga.js
+++ src/redux/user/saga.js
@@ -1,9 +1,11 @@
-import { all, takeEvery, call, put } from 'react-saga/effects'
+import { all, takeEvery, call, put, delay } from 'react-saga/effects'
 import { fetchUsersSuccess, fetchUsersFailure } from './slice'
 import axios from 'axios'
 
 function* fetchUsers() {
   try {
+    yield delay(2000)
+
     const response = yield call(
       axios.get,
       'https://jsonplaceholder.typicode.com/users/'
